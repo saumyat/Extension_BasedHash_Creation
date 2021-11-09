74b2ed06c5ce03d6f4b98e7de74bc86d1cc62958
Sammy <saumyatyagi.st@gmail.com>
2021-11-09 12:50:51 +0530

    Add files via upload

---
+++ FileCopy.ps1
@@ -0,0 +1,11 @@
+﻿"Powershell script to Copy folders"
+
+$scr = Read-Host "Please Enter Source address"
+$dest = Read-Host "Please Enter Destination Address"
+$hash = Read-Host "Enter path to hash text file"
+
+"Copying File to the destination"
+C:\Windows\System32\Robocopy.exe $scr $dest /XO /log+:log.txt
+
+"Generating Hash File"
+C:\Users\VC569MH\Downloads\hashmyfiles\HashMyFiles.exe /file "$src*.txt" /stext "$hash"
